2cd68ce527ed1806dd0eee552d454ce06ae6cc76
Kostiantyn <kostiantynmakhno@gmail.com>
2020-07-08 14:33:06 +0300

    add uart func

--- src/uart.c
+++ src/uart.c
@@ -1,3 +1,70 @@
 #include "stm32f4xx.h"                  // Device header
 #include "uart.h"
 
+void UART_Init(void)
+{
+	/* Configure PORTA PA2 - TX, PA3 - RX*/
+	if (!(RCC->AHB1ENR & RCC_AHB1ENR_GPIOAEN))
+	{
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	}
+	
+	GPIOA->MODER |= GPIO_MODER_MODE2_1; //PA2 as alternate function
+	GPIOA->MODER |= GPIO_MODER_MODE3_1; //PA3 as alternate function
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL2_0 | GPIO_AFRL_AFSEL2_1 | GPIO_AFRL_AFSEL2_2;
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL3_0 | GPIO_AFRL_AFSEL3_1 | GPIO_AFRL_AFSEL3_2;
+	
+	/* USART2 configure */
+	if (!(RCC->APB1ENR & RCC_APB1ENR_USART2EN))
+	{
+		RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+	}
+	
+	USART2->BRR = 0x14; //3 Mbps
+	USART2->CR1 |= USART_CR1_OVER8; //oversampling by 8
+	USART2->CR1 |= USART_CR1_TE; //Transmitter enable
+	USART2->CR1 |= USART_CR1_RE; //Transmitter enable
+	USART2->CR3 |= USART_CR3_DMAT; //DMA mode for transmitting
+	USART2->CR3 |= USART_CR3_DMAR; //DMA mode for receiving
+	USART2->CR1 |= USART_CR1_UE; //enable USART2
+	
+	SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_DMA1EN_Pos); //clocking DMA1
+	if (!(RCC->AHB1ENR & RCC_AHB1ENR_DMA1EN))
+	{
+		RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;
+	}
+	
+	NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+}
+
+void UART_Send(uint8_t *buff, uint32_t len)
+{
+	while (!(USART2->SR & USART_SR_TC)); //waiting for end of previous transaction
+	DMA1_Stream6->CR &= ~ DMA_SxCR_EN; //disable DMA stream
+	while (DMA1_Stream6->CR & DMA_SxCR_EN); //waiting for EN bit is reset
+	DMA1->HIFCR |= DMA_HIFCR_CTCIF6; //clear interrupt of previous transaction
+	
+	DMA1_Stream6->PAR = (uint32_t)&USART2->DR;
+	DMA1_Stream6->M0AR = (uint32_t)buff;
+	DMA1_Stream6->NDTR = len;
+	DMA1_Stream6->CR |= DMA_SxCR_CHSEL_2; //channel 4 
+	DMA1_Stream6->CR |= DMA_SxCR_PL_1; //high priority level
+	//Memory and periph data size is 8 bit, circular mode disable
+	DMA1_Stream6->CR |= DMA_SxCR_DIR_0; // mem to periph
+	DMA1_Stream6->CR |= DMA_SxCR_MINC; //memory increment mode
+	DMA1_Stream6->CR |= DMA_SxCR_TCIE; //TC interrupt enabled
+	DMA1->HIFCR |= DMA_HIFCR_CTCIF6; //clear transfer complete interrupt before new transaction
+	USART2->SR &= ~USART_SR_TC;
+	DMA1_Stream6->CR |= DMA_SxCR_EN; //activate DMA stream6 
+}
+
+
+static void DMA1_Stream6_IRQHandler(void)
+{
+	if (DMA1->HISR & DMA_HISR_TCIF6)
+	{
+		DMA1->HIFCR |= DMA_HIFCR_CTCIF6; //clear interrupt before sending
+		//DMA1_Stream6->CR &= ~DMA_SxCR_TCIE; //disable interrupt
+		GPIOD->ODR |= GPIO_ODR_OD15;
+	}
+}
